304bd1f9c76c15f00bd1cb7e163faf6832cc3ebd
Freddy Kristiansen <freddyk@microsoft.com>
2022-02-18 08:47:14 +0100

    do not remove application

--- NA/.AL-Go/NewBcContainer.ps1
+++ NA/.AL-Go/NewBcContainer.ps1
@@ -12,8 +12,8 @@ $parameters.RunSandboxAsOnPrem = $true
 
 New-BcContainer @parameters
 
-Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast | 
-    Where-Object { $_.appid -ne  "63ca2fa4-4f03-4f2b-a480-172fef340d3f" -and $_.appid -ne "437dbf0e-84ff-417a-965d-ed2bb9650972" } |
+Get-BcContainerAppInfo -containerName $parameters.ContainerName -tenantSpecificProperties -sort DependenciesLast | 
+    Where-Object { $_.appid -ne  "63ca2fa4-4f03-4f2b-a480-172fef340d3f" -and $_.appid -ne "437dbf0e-84ff-417a-965d-ed2bb9650972" -and $_.name -ne "Application" } |
     ForEach-Object {
         Write-Host "Removing $($_.Name)"
         Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
